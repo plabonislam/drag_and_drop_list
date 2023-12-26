0cbb225f0e1243ec1234fe6137fb8eaa82915e90
plabonislam <shahnurislamplabon@gmail.com>
2023-12-27 05:50:08 +0600

    test cases are divided

--- src/components/__tests__/DragAndDrop.spec.js
+++ src/components/__tests__/DragAndDrop.spec.js
@@ -6,13 +6,18 @@ import DragAndDrop from '../DragAndDrop.vue'
 describe('DragAndDrop', () => {
  it('renders properly', () => {
   const wrapper = mount(DragAndDrop)
+  expect(wrapper.html()).toMatchSnapshot();
+ })
+
+ it('title checking', () => {
+  const wrapper = mount(DragAndDrop)
   expect(wrapper.text()).toContain('Drag and drop list view');
-  expect(wrapper.find("td").exists()).toBe(true);
+ })
 
-  console.log(typeof wrapper.find('button').attributes('draggable'))
-
-  expect(wrapper.find('button').attributes('draggable')).toBe("true");
+ it('draggable btn checking', () => {
+  const wrapper = mount(DragAndDrop)
   expect(wrapper.find('button').exists()).toBe(true);
+  expect(wrapper.find('button').attributes('draggable')).toBe("true");
 
  })
 })
