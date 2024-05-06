1d3e64743930e5afb0d1c57962abe7b4039a0896
plabonislam <shahnurislamplabon@gmail.com>
2024-05-06 19:14:08 +0600

    width is expaned

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <script setup>
 import DragAndDrop from './components/DragAndDrop.vue'
+import 'vidstack/bundle';
+import { defineCustomElement,MediaCaptionsElement, MediaVideoLayoutElement } from "vidstack/elements";
+import { onMounted ,ref} from 'vue';
+
+defineCustomElement(MediaVideoLayoutElement);
+const media = ref(null)
+
+onMounted(()=>{
+  console.log(MediaVideoLayoutElement)
+})
 </script>
 
 <template>
-  <DragAndDrop />
+ <media-player title="Sprite Fight" src="https://www.youtube.com/watch?v=57C_-iMlUCU" autoplay>
+  <media-provider>
+    <media-poster
+      src="https://files.vidstack.io/sprite-fight/poster.webp"
+      alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
+    ></media-poster>
+  </media-provider>
+  <media-gesture event="pointerup" action="toggle:paused"></media-gesture>
+  <media-gesture event="dblpointerup" action="toggle:fullscreen"></media-gesture>
+  <media-gesture event="pointerup" action="toggle:controls"></media-gesture>
+  <media-gesture event="dblpointerup" action="seek:-15"></media-gesture>
+  <media-gesture event="dblpointerup" action="seek:15"></media-gesture>
+  <media-video-layout></media-video-layout>
+  <media-play-button class="media-button">
+  <!-- See "Icons" component page for setup before using the following: -->
+  <media-icon type="play" class="play-icon"></media-icon>
+  <media-icon type="pause" class="pause-icon"></media-icon>
+</media-play-button>
+</media-player>
 </template>
+
+<style scoped>
+media-player {
+  width: 600px;
+  height: 338px;
+}
+
+
+</style>
